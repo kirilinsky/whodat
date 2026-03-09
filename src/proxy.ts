08c580e5a03f50d4dd730ecad32fe29f6be99dd0
kirilinsky <kirill.ilinsky@gmail.com>
2026-03-09 11:55:48 +0100

    update protected route

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,6 +1,6 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher(["/chat(.*)", "/dashboard(.*)"]);
+const isProtectedRoute = createRouteMatcher(["/chat(.*)", "/dashboard(.*)","/profile(.*)"]);
 
 export default clerkMiddleware(async (auth, req) => {
   if (isProtectedRoute(req)) {
