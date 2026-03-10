57c9c2a904b9dc63e836ea9f6a084827061bc9dc
kirilinsky <kirill.ilinsky@gmail.com>
2026-03-10 21:32:33 +0100

    update default locale

--- src/i18n/config.ts
+++ src/i18n/config.ts
@@ -1,4 +1,4 @@
 export type Locale = (typeof locales)[number];
 
 export const locales = ["en", "ru", "de"] as const;
-export const defaultLocale: Locale = "ru";
+export const defaultLocale: Locale = "en";
